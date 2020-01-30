39a7bc2033cb99a9bbcdd232f7d5626ccf65cbb5
Ubuntu <ubuntu@ip-172-31-91-69.ec2.internal>
2020-01-30 09:04:18 +0000

    shellscript

--- cluster.sh
+++ cluster.sh
@@ -1,1 +1,7 @@
-
+#!/bin/bash
+aws emr create-cluster \
+    --release-label emr-5.14.0 \
+	--applications Name=Hadoop Name=Spark \
+	--ec2-attributes KeyName=k8s \
+    --instance-type m4.large \
+    --instance-count 2 --use-default-roles --log-uri s3://pocemr76
