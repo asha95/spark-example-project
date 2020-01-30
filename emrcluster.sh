07a0831698e550e1b69dfa61119dec4e56622f2d
asha95 <48588006+asha95@users.noreply.github.com>
2020-01-30 15:05:01 +0530

    Update emrcluster.sh

--- emrcluster.sh
+++ emrcluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 aws emr create-cluster \
-    --release-label emr-5.14.0 \
+    --release-label emr-5.29.0 --region us-east-1 \
 	--applications Name=Hadoop Name=Spark \
 	--ec2-attributes KeyName=k8s \
     --instance-type m4.large \
